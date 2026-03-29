dcfb9cfee620679bdb1dffdfc224ade676c981ea
Esposito09 <lorespo09@gmail.com>
2026-03-30 00:02:57 +0200

    created function that loads fonts for the level changing screen

--- src/levelUP.cpp
+++ src/levelUP.cpp
@@ -1,7 +1,13 @@
 //
 // Created by Admin on 29/03/2026.
 //
+#include "easy_sdl.h"
 #include "gameplay.h"
+#include "types.h"
+
+extern GameContext_t gioco;
+Index_t lvlUP;
+bool newLevel = true;
 
 bool nemiciVivi()
 {
@@ -16,6 +22,40 @@ bool nemiciVivi()
         }
     }
     return true;
+}
+
+void Load_NewLevel_Assets() {
+    if ( lvlUP.regular != NULL ) {
+        //Everything already loaded so we are going to exit
+        return;
+    }
+    lvlUP.regular = EDL_LoadAsset("assets/fonts/UbuntuMono-BoldItalic.ttf");
+    if(lvlUP.regular == NULL ) {
+        lvlUP.regular = EDL_LoadAsset("../assets/fonts/Quadrillion-Sb-It.otf");
+    }
+
+    lvlUP.space = EDL_LoadAsset("../assets/fonts/SpaceNova-6Rpd1.otf");
+    if(lvlUP.space == NULL ) {
+        lvlUP.space = EDL_LoadAsset("../assets/fonts/SpaceNova-6Rpd1.otf");
+    }
+    lvlUP.background = EDL_LoadAsset("../assets/schermate/sfondoInGame.png");
+
+    SDL_Color red = { 255, 0, 0 };
+    SDL_Color yellow = { 255, 255, 0 };
+    SDL_Color purple = { 255, 0, 255 };
+
+    lvlUP.titleStyle.font = lvlUP.regular;
+    lvlUP.titleStyle.size = 120;
+    lvlUP.titleStyle.foreground = purple;
+
+    lvlUP.menuStyle = lvlUP.titleStyle;
+    lvlUP.menuStyle.font = lvlUP.space;
+    lvlUP.menuStyle.size = 100;
+    lvlUP.menuStyle.foreground = yellow;
+
+    lvlUP.selectedStyle = lvlUP.menuStyle;
+    lvlUP.selectedStyle.foreground = red;
+
 }
 
 void levelUP()
