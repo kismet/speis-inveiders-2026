925884ae4dbe52348f00d02ea218031f02d6ec14
pandolfimarco <pandolfimarco816@gmail.com>
2026-03-29 12:36:10 +0200

    changed the backgrond screen, finish the restart button in gameOver

--- src/reset_status_play.cpp
+++ src/reset_status_play.cpp
@@ -5,13 +5,33 @@
 #include "gameplay.h"
 #include "easy_sdl.h"
 #include "types.h"
-#include <optional>
 
 using namespace std;
 
 extern GameContext_t gioco;
+extern char tabellone[27][23];
+extern Player_t player;
+extern bool versoDestra;
 
 void resetStatusPlay()
 {
+    player.punteggio=0;
+    player.x=11;
 
+    for (int i=0; i<RIGHE; i++)
+    {
+        for (int j=0; j<COLONNE; j++)
+        {
+            tabellone[i][j]=VUOTO_SYMBOL;
+        }
+    }
+    for (int i = 0; i < 8; i++) {
+        for (int j = 7; j <= 15; j++) {
+            tabellone[i][j] = NEMICO_SYMBOL;
+        }
+    }
+
+    tabellone[26][11]=NAVICELLA_SYMBOL;
+
+    gioco.stato=GAME_STATUS_PLAY;
 }
