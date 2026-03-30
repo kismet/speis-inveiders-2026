88751ed06601f4dc2430622e52633fd8c414e020
Esposito09 <lorespo09@gmail.com>
2026-03-31 01:13:47 +0200

    updated "globals.h" to contain every extern used in cpp files

--- include/globals.h
+++ include/globals.h
@@ -5,4 +5,16 @@
 #ifndef SPEIS_INVEIDERS_GLOBALS_H
 #define SPEIS_INVEIDERS_GLOBALS_H
 
+#include "types.h"
+
+extern bool versoDestra;
+extern int startTime;
+extern unsigned int missile_time;
+extern unsigned int movement_time;
+extern Player_t player;
+extern GameContext_t gioco;
+extern bool versoDestra;
+extern const char TABELLONE[27][23];
+extern char tabellone[27][23];
+
 #endif //SPEIS_INVEIDERS_GLOBALS_H
